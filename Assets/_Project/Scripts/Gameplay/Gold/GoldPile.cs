97943e6a9397cf19c047228b712a22c8d1b79fc0
RomanT <roman.tara@holydaystudios.com>
2026-04-25 19:31:06 +0700

    click on gold added

---
+++ Assets/_Project/Scripts/Gameplay/Gold/GoldPile.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Utils;
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay.Gold
+{
+    public class GoldPile : ClickableView<GoldPile>
+    {
+        
+        public override void Awake()
+        {
+            OnClicked += DoLog;
+        }
+
+        private void DoLog(GoldPile obj)
+        {
+            Debug.Log("GoldPile clicked");
+        }
+
+        private void OnDestroy()
+        {
+            OnClicked -= DoLog;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Shared/ClickableView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _Project.Scripts.Utils
+{
+    public abstract class ClickableView<T> : MonoBehaviour where T : ClickableView<T>
+    {
+        protected Collider2D _collider2D;
+
+        public event Action<T> OnClicked = delegate { };
+
+        public virtual void Awake()
+        {
+            _collider2D = GetComponent<Collider2D>();
+        }
+
+        public void DisableInteract()
+        {
+            _collider2D.enabled = false;
+        }
+
+        public virtual void OnMouseDown()
+        {
+            if (IsPointerOverUI())
+                return;
+
+            Interact();
+        }
+
+        private bool IsPointerOverUI()
+        {
+            return EventSystem.current != null && EventSystem.current.IsPointerOverGameObject();
+        }
+
+        protected virtual void Interact()
+        {
+            OnClicked.Invoke(this as T);
+        }
+    }
+}
