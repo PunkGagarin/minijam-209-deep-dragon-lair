aa4c1d41f82de3094b316d01202cb8b319b9db41
RomanT <roman.tara@holydaystudios.com>
2026-04-27 02:37:13 +0700

    crows added

--- Assets/_Project/Scripts/Gameplay/Units/UnitSpawner.cs
+++ Assets/_Project/Scripts/Gameplay/Units/UnitSpawner.cs
@@ -4,22 +4,35 @@ namespace _Project.Scripts.Gameplay.Units
 {
     public class UnitSpawner : MonoBehaviour
     {
-        [SerializeField] private Unit _unitPrefab;
-        [SerializeField] private Transform _guildPoint;
-        [SerializeField] private Transform _gatherGoldPoint;
+        [SerializeField]
+        private Unit _unitPrefab;
 
-        public bool CanSpawn => _unitPrefab != null && _guildPoint != null && _gatherGoldPoint != null;
+        [SerializeField]
+        private Unit _crowPrefab;
+
+        [SerializeField]
+        private Transform _guildPoint;
+
+        [SerializeField]
+        private float _yCrowOffset = 1f;
+
+        [SerializeField]
+        private Transform _gatherGoldPoint;
+
+        public bool CanSpawn =>
+            _unitPrefab != null && _crowPrefab != null && _guildPoint != null && _gatherGoldPoint != null;
 
         public void SetGatherGoldPoint(Transform gatherGoldPoint)
         {
             _gatherGoldPoint = gatherGoldPoint;
         }
 
-        public Unit SpawnUnit(float mineTime)
+        public Unit SpawnUnit(float mineTime, UnitType type)
         {
             if (_unitPrefab == null)
             {
-                Debug.LogError($"{nameof(UnitSpawner)} on {name} cannot spawn a unit because Unit prefab is not assigned.", this);
+                Debug.LogError(
+                    $"{nameof(UnitSpawner)} on {name} cannot spawn a unit because Unit prefab is not assigned.", this);
                 return null;
             }
 
@@ -30,9 +43,17 @@ namespace _Project.Scripts.Gameplay.Units
                     this);
                 return null;
             }
-
-            Unit unit = Instantiate(_unitPrefab, _guildPoint.position, Quaternion.identity);
-            unit.Initialize(_guildPoint, _gatherGoldPoint, mineTime);
+            Unit unit = null;
+            if (type == UnitType.Unit)
+            {
+                unit = Instantiate(_unitPrefab, _guildPoint.position, Quaternion.identity);
+                unit.Initialize(_guildPoint, _gatherGoldPoint, mineTime);
+            }
+            else
+            {
+                unit = Instantiate(_crowPrefab, _guildPoint.position + new Vector3(0f, _yCrowOffset, 0f), Quaternion.identity);
+                unit.Initialize(_guildPoint, _gatherGoldPoint, mineTime, _yCrowOffset);
+            }
             return unit;
         }
     }
